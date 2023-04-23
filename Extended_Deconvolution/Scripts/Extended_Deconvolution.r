4536f8248096d55c58d2c097a06929237df0396f
jiqingchen <40001947+jiqingchen@users.noreply.github.com>
2023-04-23 12:59:20 -0400

    Add files via upload

---
+++ Extended_Deconvolution/Scripts/Extended_Deconvolution.r
@@ -0,0 +1,2 @@
+library(FlowSorted.Blood.EPIC)
+Extended<-round(projectCellType_CP(Beta_matrix[rownames(FlowSorted.BloodExtended.EPIC.compTable),], FlowSorted.BloodExtended.EPIC.compTable,lessThanOne =T)*100,2)
